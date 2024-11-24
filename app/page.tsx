1744a1f0c44962ae7e617608ccbdbe88a79f5514
da-shina <139605659+da-shina@users.noreply.github.com>
2024-11-24 16:45:48 +0900

    add images

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 import styles from "./page.module.css";
 
 export default function Home() {
-  const name="世界1";
+  const name="世界";
   return (
     <>
     <h1>テクノロジーの力で{name}を変える</h1>
